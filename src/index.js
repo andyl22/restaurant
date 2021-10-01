ad1f2793bdad767f8b82103323972bd9a4577246
Andy Lau <landywl4@gmail.com>
2021-09-30 22:04:09 -0700

    Set up Initial Styling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+import "./index.css";
+
 import ChristmasCookies from './christmas-cookies.jpg'
+import Background from './Background.jpg'
 
 const content = document.getElementById("content");
 
 createHeader();
-createImage();
+// createImage();
 
 function createHeader() {
+    const headerContainer = document.createElement("header");
     const header = document.createElement("h1");
     const subHeader = document.createElement("h2");
     header.textContent = "The Cookie Factory";
     subHeader.textContent = "World Class Cookies, 12 years in a row!";
-    content.appendChild(header);
-    content.appendChild(subHeader);
+    headerContainer.appendChild(header);
+    headerContainer.appendChild(subHeader);
+    content.appendChild(headerContainer);
 }
 
 function createImage() {
