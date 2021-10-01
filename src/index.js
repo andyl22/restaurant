117710607703ed477e13506ecf8551d3e5296207
Andy Lau <landywl4@gmail.com>
2021-09-30 23:19:32 -0700

    Added tabs functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,78 @@
 import "./index.css";
-
 import ChristmasCookies from './christmas-cookies.jpg'
-import Background from './Background.jpg'
 
 const content = document.getElementById("content");
-
-createHeader();
-// createImage();
+createHeader(content);
 
 function createHeader() {
-    const headerContainer = document.createElement("header");
-    const header = document.createElement("h1");
-    const subHeader = document.createElement("h2");
-    header.textContent = "The Cookie Factory";
-    subHeader.textContent = "World Class Cookies, 12 years in a row!";
-    headerContainer.appendChild(header);
-    headerContainer.appendChild(subHeader);
-    content.appendChild(headerContainer);
+    const headerContent = document.createElement("header");
+    createHeaderText();
+    createTabs();
+
+    function createHeaderText() {
+        const header = document.createElement("h1");
+        const subHeader = document.createElement("h2");
+        header.textContent = "The Cookie Factory";
+        subHeader.textContent = "World Class Cookies, 12 years in a row!";
+        headerContent.appendChild(header);
+        headerContent.appendChild(subHeader);
+    }
+
+    function createTabs() {
+        headerContent.appendChild(createTabContainer());
+
+        function createTabContainer() {
+            const tabsContainer = document.createElement("div");
+            tabsContainer.setAttribute("id", "tabsContainer");
+            return createTabElements(tabsContainer);
+
+        }
+
+        function createTabElements(tabsContainer) {
+            addToContainer(
+                addButtonText(
+                    addSelectors(
+                        declareTabs()
+                    )
+                )
+            );
+            return tabsContainer;
+
+            function declareTabs() {
+                let tabOne = document.createElement("button");
+                let tabTwo = document.createElement("button");
+                let tabThree = document.createElement("button");
+                return [tabOne, tabTwo, tabThree];
+            }
+
+            function addSelectors(tabsArray) {
+                let idCount = 0;
+                tabsArray.forEach(tab => tab.classList.add("tabs"));
+                tabsArray.forEach(tab => {
+                    tab.setAttribute("id", "tab" + idCount);
+                    idCount++;
+                })
+                return tabsArray;
+            }
+
+            function addButtonText(tabsArray) {
+                tabsArray[0].textContent = "Home";
+                tabsArray[1].textContent = "Menu";
+                tabsArray[2].textContent = "Contact";
+                return tabsArray;
+            }
+
+            function addToContainer(tabsArray) {
+                tabsArray.forEach(tab => tabsContainer.appendChild(tab));
+            }
+        }
+    }
+
+    content.appendChild(headerContent);
 }
 
-function createImage() {
-    const myImage = new Image();
-    myImage.src = ChristmasCookies;
-    content.appendChild(myImage);
-}
+// function createImage() {
+//     const myImage = new Image();
+//     myImage.src = ChristmasCookies;
+//     content.appendChild(myImage);
+// }
