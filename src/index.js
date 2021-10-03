5e7f14afa6f09b2ab09cd4f4c55401eabf0abe86
Andy Lau <landywl4@gmail.com>
2021-10-03 12:35:08 -0700

    Finalizing website style and added content management through Javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import "./index.css";
+import "./index.css"
+import cookiesWithDrinkImage from "./danish-with-drinks.jpg"
+import warmCookies from "./warm-cookies.jpg"
+import sanFranImage from "./san-francisco.jpg"
+import bakeryImage from "./bakery.jpg"
+import pumpkinSpiceCupcakes from "./pumpkin-spice-cupcakes.jpg"
+import pumpkinPie from "./pumpkin-pie.jpg"
 
 if (process.env.NODE_ENV == 'production') {
     console.log('Production mode!');
@@ -15,15 +21,14 @@ function createHeader() {
     function createHeaderText() {
         const pageTitle = document.createElement("h1");
         const subTitle = document.createElement("h2");
-        pageTitle.textContent = "The Cookie Factory";
-        subTitle.textContent = "World Class Cookies, 12 years in a row!";
+        pageTitle.textContent = "Andy's Bakery";
+        subTitle.textContent = "Autumn Specials Now In Store!";
         appendTo(header, pageTitle);
         appendTo(header, subTitle);
     }
 
     function createTabs() {
-        appendTo(header, createTabContainer() );
-
+        appendTo(header, createTabContainer());
         function createTabContainer() {
             const tabsButtonContainer = document.createElement("div");
             tabsButtonContainer.setAttribute("id", "tabs-button-container");
@@ -60,7 +65,7 @@ function createHeader() {
 
             function addButtonText(tabsArray) {
                 tabsArray[0].textContent = "Home";
-                tabsArray[1].textContent = "Seasonal";
+                tabsArray[1].textContent = "Menu";
                 tabsArray[2].textContent = "Contact";
                 return tabsArray;
             }
@@ -74,37 +79,37 @@ function createHeader() {
 }
 
 
-function addFunctionalityToTabs() {
-    const tabFactory = tabId => {
-        let contentHeader = (tabId == "tab0") ? tab0() :
-        (tabId == "tab1") ? tab1() :
-        tab2();
+function addTabFunctionAndContent() {
+    const createTabContent = tabId => {
+        let contentHeader = (tabId == "tab0") ? homeTabContentHeader() :
+        (tabId == "tab1") ? menuTabContentHeader() :
+        contactTabContentHeader();
 
         let contentContainer = createContentContainer();
 
-        let content = (tabId == "tab0") ? content0() :
-        (tabId == "tab1") ? content1() :
-        content2();
+        let content = (tabId == "tab0") ? homeTabContent() :
+        (tabId == "tab1") ? menuTabContent() :
+        contactTabContent();
 
         content.forEach(element => contentContainer.appendChild(element));
         return [contentHeader, contentContainer];
 
-        function tab0() {
-            const aboutHeader = document.createElement("h3");
-            aboutHeader.textContent = "Homemade Cookies from San Francisco since 2014";
-            return aboutHeader;
+        function homeTabContentHeader() {
+            return h3Text("Homemade Cookies from San Francisco!");
         }
 
-        function tab1() {
-            const menuHeader = document.createElement("h3");
-            menuHeader.textContent = "Our Seasonal Cookies";
-            return menuHeader;
+        function menuTabContentHeader() {
+            return h3Text("Our Seasonal Cookies!");
         }
         
-        function tab2() {
-            const contactHeader = document.createElement("h3");
-            contactHeader.textContent = "Contact Us Now!";
-            return contactHeader;
+        function contactTabContentHeader() {
+            return h3Text("Contact Us Now!");
+        }
+
+        function h3Text(string) {
+            const h3Text = document.createElement("h3");
+            h3Text.textContent = string;
+            return h3Text;
         }
 
         function createContentContainer() {
@@ -113,28 +118,70 @@ function addFunctionalityToTabs() {
             return subContainer;
         }
 
-        function content0() {
-            let contentArray = [];
-            let headerParagraph = document.createElement("p");
-            headerParagraph.textContent = "Homemade Cookies from San Francisco since 2014";
-            contentArray.push(headerParagraph);
+        function homeTabContent() {
+            const contentArray = [];
+            contentArray.push(h2Text("Homemade Bakery from San Francisco"));
+            contentArray.push(articleText("Come check out our freshly baked goods today in the Excelsior District, in San Francisco. <br>"));
+            contentArray.push(contentImage(bakeryImage, "", "home-page"));
             return contentArray;
         }
 
-        function content1() {
-            let contentArray = [];
-            let headerParagraph = document.createElement("p");
-            headerParagraph.textContent = "Our Seasonsal Cookies";
-            contentArray.push(headerParagraph);
+        function menuTabContent() {
+            const contentArray = [];
+            contentArray.push(h2Text("Our Seasonal Items"));
+            contentArray.push(articleText("Warm up with our Fall drinks, with these seasonal specials."));
+            contentArray.push(contentImageArray({"Cookies With Pumpkin Spice Coffee": cookiesWithDrinkImage, "Melted Cookies": warmCookies}));
+            contentArray.push(contentImageArray({"Pumpkin Spice Cupcakes": pumpkinSpiceCupcakes, "Pumpkin Pie": pumpkinPie}));
             return contentArray;
         }
         
-        function content2() {
-            let contentArray = [];
-            let headerParagraph = document.createElement("p");
-            headerParagraph.textContent = "Contact Us Now!";
-            contentArray.push(headerParagraph);
+        function contactTabContent() {
+            const contentArray = [];
+            contentArray.push(h2Text("Contact Us Now!"));
+            contentArray.push(contentImage(sanFranImage));
+            contentArray.push(articleText("123 Baker Street"));
             return contentArray;
+        }
+
+        function h2Text(string) {
+            const headerText = document.createElement("h2");
+            headerText.textContent = string;
+            return headerText;
+        }
+
+        function articleText(string) {
+            const paragraphText = document.createElement("p");
+            paragraphText.innerHTML = string;
+            return paragraphText;
+        }
+
+        function contentImage(imageURL, figCaption="", idValue="") {
+            const figure = document.createElement("figure");
+            const myImage = new Image();
+            myImage.src = imageURL;
+            figure.append(myImage);
+
+            if (figCaption) {
+                const caption = document.createElement("figcaption");
+                caption.textContent = figCaption;
+                figure.append(caption);
+            }
+
+            if (idValue) {figure.setAttribute("id", idValue)}
+            
+            figure.classList.add("images");
+
+            return figure;
+        }
+
+        function contentImageArray(figures) {
+            const imageContainer = document.createElement("div");
+            for (let key in figures) {
+                let val = figures[key];
+                imageContainer.classList.add("figures-container");
+                imageContainer.appendChild(contentImage(val, key));
+            }
+            return imageContainer;
         }
     }
 
@@ -143,7 +190,7 @@ function addFunctionalityToTabs() {
     
     function initialTab() {
         let contentContainer = createTabContainer();
-        let tab = tabFactory("tab0");
+        let tab = createTabContent("tab0");
         tab.forEach(element => contentContainer.appendChild(element));
         appendTo(main, contentContainer);
         document.getElementById("tab0").classList.add("selected");
@@ -152,18 +199,18 @@ function addFunctionalityToTabs() {
     function addListeners() {
         let idCount = 0;
         while (idCount<3) {
-            document.getElementById("tab"+idCount).addEventListener("click", selectTab)
+            document.getElementById("tab"+idCount).addEventListener("click", changeToSelectedTab)
             idCount++;
         }
     }
 
-    function selectTab(e) {
+    function changeToSelectedTab(e) {
         removeExistingContent();
         let tabContainer = createTabContainer();
         let tabId = e.target.getAttribute("id");
-        let tab = tabFactory(tabId)
-        tabContainer.appendChild(tab[0]);
-        tabContainer.appendChild(tab[1]);
+        let tabContent = createTabContent(tabId)
+        tabContainer.appendChild(tabContent[0]);
+        tabContainer.appendChild(tabContent[1]);
         appendTo(main,tabContainer);
         e.target.classList.add("selected");
     }
@@ -191,4 +238,4 @@ function appendTo(appendee, appending) {
 const main = document.getElementsByTagName("main")[0];
 const header = document.getElementsByTagName("header")[0];
 createHeader();
-addFunctionalityToTabs();
+addTabFunctionAndContent();
